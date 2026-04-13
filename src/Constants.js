89b2007e82df600fc479842de2596d41c8792dd9
PrexxaSAS <la.perez@prexxa.com.co>
2026-04-12 22:23:28 -0500

    Create Constants.js

---
+++ src/Constants.js
@@ -0,0 +1,10 @@
+export const P = {
+  950: "#1e0050", 900: "#3b0764", 800: "#4c1d95", 700: "#6d28d9",
+  600: "#7c3aed", 500: "#8b5cf6", 400: "#a78bfa", 300: "#c4b5fd",
+  200: "#ddd6fe", 100: "#ede9fe", 50:  "#f5f3ff",
+};
+
+export const CIUDADES = [
+  { code: "11001", name: "Bogotá D.C." }, { code: "05001", name: "Medellín" },
+  { code: "76001", name: "Cali" }, { code: "08001", name: "Barranquilla" }
+];
