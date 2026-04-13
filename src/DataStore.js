8dfa94a8595f9c97d8c3fda20f32475c3f62a278
PrexxaSAS <la.perez@prexxa.com.co>
2026-04-13 14:49:01 -0500

    Delete src/DataStore.js

--- src/DataStore.js
+++
@@ -1,6 +0,0 @@
-// Simulación de base de datos en memoria
-export const store = {
-  pedidos: [], // Array de objetos pedido
-  usuarios: [], // Array de usuarios (roles: admin, conductor, cliente)
-  transportistas: [], // Transportistas y sus conductores vinculados
-};
