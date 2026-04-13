2c6c781d1754ab829291ee88ace6e6acd6ed1062
PrexxaSAS <la.perez@prexxa.com.co>
2026-04-12 21:09:00 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import SomosProTracking from './SomosProTracking'; 
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SomosProTracking from './SomosProTracking'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <SomosProTracking />
   </React.StrictMode>
-);
+)
